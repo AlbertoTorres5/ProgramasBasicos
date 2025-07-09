11c030d7eb8d9ef0bae0a0746516a99c481a3d2b
AlbertoTorres5 <c.albertots25@gmail.com>
2025-07-09 01:36:27 -0600

    Agregando programa de tabla de multiplicar

---
+++ TablaMultiplicar.py
@@ -0,0 +1,7 @@
+#Ingresa la tabla que deseas realizar
+num = int(input("Tabla que deseas mostrar: "))
+
+#Uso de bucle para realizar la tabla del 1 al 10
+for i in range(1, 11):
+    print(f"{num} x {i} = {num * i}")
+
