985ece045e339fb02454ca44b75c8a989545a9fd
AlbertoTorres5 <c.albertots25@gmail.com>
2025-07-12 01:39:56 -0600

    Agregar programa de conversion de grados

---
+++ CelsiusFahrenheit.py
@@ -0,0 +1,8 @@
+#Solicita al usuario que ingrese una temperatura en grados Celsius
+celsius = float(input("Ingresa una temperatura en grados Celsius: "))
+
+#Conversión de Celsius a Farenheit
+fahrenheit = (celsius * 9 / 5) + 32
+
+#Muestra de temperatura ya convertida
+print("Temperatura en grados Fahrenheit: ", fahrenheit)
